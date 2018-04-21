0ab754af7469bdefd91350e4495a9bc1a434f3c1
Sand Sand <saibaoomfu@gmail.com>
2018-04-22 00:31:19 +0200

    props and component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-setInterval(function(){
+function Cartoon(props) {
+	return <h1>hello, {props.name} on {props.show}</h1>
+}
+// class Cartoon extends React.Component{
+// 	render(){
+// 		return <h1>Hello,{this.props.name}</h1>
+// 	}
+// }
+function Show(){
+	return <div>
+						<Cartoon name="Sai" show="Pokemoon"/>
+						<Cartoon name="Jassmine" show="Alladin"/>
+					</div>
+}
 ReactDOM.render(
-	<h2>Time now- {new Date().toLocaleTimeString()}</h2>,
+	 <Show/>,
 	document.getElementById('root')
 	);
-},1000);
