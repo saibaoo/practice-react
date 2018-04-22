f0cfdb49f4148bf46fd3890c046410557919c12b
Sand Sand <saibaoomfu@gmail.com>
2018-04-22 14:34:33 +0200

    list items

--- src/index.js
+++ src/index.js
@@ -1,35 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function Message(props){
-	if(props.value){
-			return <h1>This is first message</h1>
-	}
-		return <h1>I am from second message</h1>
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+function ToonList(props){
+	const list =props.cartoon
+	// const toons = list.map((list,index)=><li key={index}>{list}</li>)
+	return <ul>list.map((list,index)=><li key={index}>{list}</li>)</ul>
 }
-class Btn extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {value:true}
-	}
 
-	handleClick = () =>{
-		this.setState({
-			value: !this.state.value
-		});
-	}
+const cartoons = ['Pickchu','Balidin', 'tom','tom']
 
-	render(){
-		return (
-			<div>
-				<button onClick={this.handleClick}>Change the Message</button>
-				<Message value={this.state.value}/>
-			</div>
-		)
 
-	}
-}
 ReactDOM.render(
-	<Btn/>,
-document.getElementById('root')
+	<ToonList cartoon ={cartoons}/>,
+	document.getElementById('root')
 );
