905204710aea00f7fca14ade13daf2c454957fd9
Sand Sand <saibaoomfu@gmail.com>
2018-04-22 14:38:17 +0200

    corrected list item

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom'
 function ToonList(props){
 	const list =props.cartoon
 	// const toons = list.map((list,index)=><li key={index}>{list}</li>)
-	return <ul>list.map((list,index)=><li key={index}>{list}</li>)</ul>
+	return <ul>
+{	list.map((list,index)=><li key={index}>{list}</li>)}
+	</ul>
 }
 
 const cartoons = ['Pickchu','Balidin', 'tom','tom']
